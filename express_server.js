be44fba1895ffdf357502ddadb0df251bf349feb
amaninders <amandeolofficial@gmail.com>
2021-08-31 04:09:24 +0000

    implemented the body parser to retrieve user input from new url form

--- express_server.js
+++ express_server.js
@@ -2,14 +2,21 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 // set view engine to ejs
 app.set('view engine','ejs');
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+	return Math.random().toString(36).substring(2, 8);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -31,6 +38,11 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
