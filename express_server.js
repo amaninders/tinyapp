cbf841262e672a69a79b5d21741681aaa15769c9
amaninders <amandeolofficial@gmail.com>
2021-08-31 03:09:19 +0000

    enabled a ejs template for urls and passed variable

--- express_server.js
+++ express_server.js
@@ -19,8 +19,9 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index.ejs", templateVars);
 });
 
 app.listen(PORT, () => {
