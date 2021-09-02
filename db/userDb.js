195385354b5e44c20335bd107c04454fc9ac4a1b
amaninders <amandeolofficial@gmail.com>
2021-09-02 16:24:05 +0000

    moved storagre objects into db directory

---
+++ db/userDb.js
@@ -0,0 +1,17 @@
+// users database
+const users = {
+  "aJ48lW": {
+    id: "aJ48lW",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+module.exports = {
+	users
+}
