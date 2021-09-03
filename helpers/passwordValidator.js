85fe4a17f503b6fb940c5a577b21f249886dacb2
amaninders <amandeolofficial@gmail.com>
2021-09-03 01:54:55 +0000

    fixed the function for password validation

--- helpers/passwordValidator.js
+++ helpers/passwordValidator.js
@@ -1,7 +1,7 @@
 // validate password
 const validPassword = input => {
-  const pwRegex =  /^[A-Za-z]\w{0,20}$/;
-  return (input !== undefined && input.match(pwRegex) ? true : false);
+  const pwRegex =  /^[A-Za-z]\w{6,20}$/;
+  return (input !== undefined && pwRegex.test(input) ? true : false);
 };
 
 module.exports = {
