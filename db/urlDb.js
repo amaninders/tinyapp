1fccb4cf7a99bdd1b4b5407b4f2dd94b1ae952d0
amaninders <amandeolofficial@gmail.com>
2021-09-05 08:07:26 +0000

    implemented new db structure

--- db/urlDb.js
+++ db/urlDb.js
@@ -2,11 +2,19 @@
 const urlDB = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
-    userID: "aJ48lW"
+    userID: "aJ48lW",
+		created_on: null,
+		last_visit: null,
+		total_visit: '0',
+		unique_visit: '0'
   },
   i3BoGr: {
     longURL: "https://www.google.ca",
-    userID: "aJ48lW"
+    userID: "aJ48lW",
+		created_on: null,
+		last_visit: null,
+		total_visit: '0',
+		unique_visit: '0'
   }
 };
 
